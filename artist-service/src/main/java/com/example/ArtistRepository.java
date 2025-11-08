59f2f51e458bed5eb2c8f31bf15ef2d9687bae04
shillxzy <bzae03014@.com>
2025-11-08 21:46:03 +0200

    Lab3: Complete redesign of the project structure. Replacement of conventional folder services with project services to meet the requirements of microservice architecture. Addition of Dockerfile and main dockercompose.yml.

---
+++ artist-service/src/main/java/com/example/ArtistRepository.java
@@ -0,0 +1,14 @@
+package com.example.services;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import java.util.List;
+
+@ApplicationScoped
+public class ArtistRepository {
+    public List<Artist> getAllArtists() {
+        return List.of(
+                new Artist(1, "Artist X", "Pop"),
+                new Artist(2, "Artist Y", "Rock")
+        );
+    }
+}
---
+++ user-service/src/main/java/com/example/UserController.java
@@ -0,0 +1,22 @@
+package com.example.services;
+
+import io.smallrye.config.WithDefault;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.inject.Inject;
+import java.util.List;
+
+@Path("/users")
+@Produces(MediaType.APPLICATION_JSON)
+public class UserController {
+
+    @Inject
+    UserRepository userRepository;
+
+    @GET
+    public List<User> getAllUsers() {
+        return userRepository.getAllUsers();
+    }
+}
